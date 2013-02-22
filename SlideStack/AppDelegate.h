6fb5b31e28102dda314a5b5fab186816675a3e8a
himara2 <himaratsu@gmail.com>
2013-02-23 00:31:17 +0900

    Initial Commit

---
+++ SlideStack/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  SlideStack
+//
+//  Created by 平松 亮介 on 2013/02/23.
+//  Copyright (c) 2013年 mashroom. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ SlideStack/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  SlideStack
+//
+//  Created by 平松 亮介 on 2013/02/23.
+//  Copyright (c) 2013年 mashroom. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
